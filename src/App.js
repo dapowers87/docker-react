ef1e0bdf3c669730477d076136c5d6d5e4dc33fe
David Powers <david.powers@newgistics.com>
2019-08-09 12:21:27 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testasdf
         </p>
+        This is the final test
         <a
           className="App-link"
           href="https://reactjs.org"
